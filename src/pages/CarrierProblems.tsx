f216c82fc5d2e5f848155784aaf83fd7c89d7162
Sirpathi  Naveen <naveen.sirpathi@cjsstech.com>
2025-12-02 09:13:20 +0530

    Naveen modified

--- src/pages/CarrierProblems.tsx
+++ src/pages/CarrierProblems.tsx
@@ -1,36 +1,7 @@
-// import CareerNavigationBar from '@/components/Career/CareeerNavigationBar';
-
-// // import React from 'react';
-// interface CarrierProblemsProps {
-//   navigateTo: (page: string) => void;
-// }
-// const CarrierProblems = ({ navigateTo }: CarrierProblemsProps) => {
-//   const links = [
-//     { label: 'Home', page: 'CarrierHome' },
-//     { label: 'Life at CJSS', page: 'CarrierLife' }, // 👈 must match App key
-//     { label: 'Job for Fresher', page: 'CarrierFresher' }, // or whatever you use
-//     { label: 'Job for Experienced', page: 'CarrierExperienced' },
-//     { label: 'Problems we solve', page: 'CarrierProblems' },
-//   ];
-
-//   return (
-//     <>
-//       <CareerNavigationBar links={links} navigateTo={navigateTo} />
-//       <section className="w-full h-28">
-//         <p>Career life</p>
-//       </section>
-//     </>
-//   );
-// };
-
-// export default CarrierProblems;
-
 'use client';
 
 import CareerNavigationBar from '@/components/Career/CareeerNavigationBar';
 import {
-  ArrowRight,
-  BarChart3,
   CloudCog,
   Gauge,
   LineChart,
