75c939377f8fd8105fe7f14ae4752a0125991862
Rakesh Munukuntla <rakesh9490434912@gmail.com>
2026-02-06 12:40:39 +0530

    06-02-2026 12:40

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -136,7 +136,7 @@ export const Footer = ({ navigateTo }: FooterProps) => {
                 </li>
 
                 {/* Twitter */}
-                <li>
+                {/* <li>
                   <a
                     href="https://twitter.com/CJSS8102021"
                     target="_blank"
@@ -179,9 +179,9 @@ export const Footer = ({ navigateTo }: FooterProps) => {
                       <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53A4.48 4.48 0 0 0 12 8v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" />
                     </svg>
                   </a>
-                </li>
+                </li> */}
 
-                {/* Facebook */}
+                {/* Facebook
                 <li>
                   <a
                     href="https://www.facebook.com/Customer-Journey-Software-Solutions-112603838118106"
@@ -224,7 +224,7 @@ export const Footer = ({ navigateTo }: FooterProps) => {
                       <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
                     </svg>
                   </a>
-                </li>
+                </li> */}
               </ul>
             </div>
           </div>
